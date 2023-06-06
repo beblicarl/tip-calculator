9b84372b473a84a0020225a44a922404b2ec9ee7
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-06 14:26:01 +0000

    Amount component

---
+++ src/components/Amount.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+
+export default function Amount(props) {
+  return (
+    <div className="amount">
+      <div className="amount-child">
+        <h3>
+          Tip Amount <br /> <span>/person</span>
+        </h3>
+        <div>
+          <p className="value">${props.tipAmount.toFixed(2)}</p>
+        </div>
+      </div>
+
+      <div className="amount-child">
+        <h3>
+          Total <br /> <span>/person</span>
+        </h3>
+        <div>
+          <p className="value">${props.totalAmount.toFixed(2)}</p>
+        </div>
+      </div>
+      {props.isSubmitted ? (
+        <button onClick={props.handleRefresh}>REFRESH</button>
+      ) : (
+        <button onClick={props.handleTotalAmount}>SUBMIT</button>
+      )}
+    </div>
+  );
+}
