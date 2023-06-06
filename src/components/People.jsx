29315b4625a622dd82aa7c883c5c6989327bac83
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-06 19:42:54 +0000

    added error message to the form input

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -6,12 +6,14 @@ export default function NumberOfPeople(props) {
     <div className="people-number">
       <p>Number of People</p>
       <FormInput
+        {...props.register(props.name, { required: true, min: 1 })}
         type={props.type}
         name={props.name}
         id={props.id}
         value={props.value}
         onChange={props.onChange}
       ></FormInput>
+      {props.errors.people && <span>This field is required</span>}
     </div>
   );
 }
