118af92b55db8cf25b506bcae62e51d85cdfea70
Carl Bebli <beblicarl.cb@gmail.com>
2023-06-13 13:46:52 +0000

    passed down registration and error prop to the bill component

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -9,8 +9,8 @@ export default function Bill(props) {
         type={props.type}
         name={props.name}
         id={props.id}
-        value={props.value}
-        onChange={props.onChange}
+        registration={props.registration}
+        error={props.error}
       ></FormInput>
     </div>
   );
